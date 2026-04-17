3470ee934d30c9a9318c6b1de1f78daec26a966c
aye2-win <ayyayywin02@gmail.com>
2026-04-17 19:25:45 +0630

    add role-based routes and auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Routes>
 
-export default App
+      {/* public routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* user protected route */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* admin protected route */}
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute adminOnly>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* unknown routes fallback */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+
+    </Routes>
+  );
+};
+
+export default App;
